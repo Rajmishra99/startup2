8c3ddfaffd0add3935a8474afbcbd51c2e3afc43
Rajkumar Mishra <130075988+Rajmishra99@users.noreply.github.com>
2025-06-25 14:53:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggleMenu() {
   const nav = document.getElementById('navLinks');
   nav.classList.toggle('show');
 }
+// Fade-in on scroll
+window.addEventListener('scroll', () => {
+  const about = document.querySelector('.about-container');
+  const sectionTop = about.getBoundingClientRect().top;
+  const screenPosition = window.innerHeight / 1.3;
+
+  if (sectionTop < screenPosition) {
+    about.classList.add('show');
+  }
+});
