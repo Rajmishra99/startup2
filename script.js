f26a86998e1796a5b2906b9c20e89ba6246bbaad
Rajkumar Mishra <130075988+Rajmishra99@users.noreply.github.com>
2025-06-25 23:32:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-function toggleMenu() {
-  const nav = document.getElementById('navLinks');
-  nav.classList.toggle('show');
-}
-
 window.addEventListener('scroll', () => {
   const section = document.querySelector('.about-container');
   const top = section.getBoundingClientRect().top;
-  const screenPosition = window.innerHeight;
+  const screenHeight = window.innerHeight;
 
-  if (top < screenPosition - 100) {
+  if (top < screenHeight - 100) {
     section.classList.add('show');
   }
 });
-
-
